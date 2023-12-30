f3782ca2a8916bb036a18197b729e448a3c29a24
ito493001 <114464389+ito493001@users.noreply.github.com>
2023-12-30 20:48:36 +0900

    Update streamlit_app.py
    アップロードしたアクセスログファイルの読み込み

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
-import streamlit as st
-uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
+import pandas as pd
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
